7bf8ce87453c160b9610cce1edde927c01086b3f
Pedro Henrique <pedro00dk@live.com>
2015-10-17 15:06:57 -0300

    Modificação no main para gerar as classes durante os testes

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,28 @@
-import miniJava.lexer.Lexer;
+import miniJava.util.Gen;
 
-import java.io.IOException;
-import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
-/**
- * Main criado para testar o analizador
- */
 public class Main {
+
+    // Caminho para as especificações do lexer a parser
+    public static final Path LEXER_SPEC_PATH = Paths.get("src\\miniJava\\lexer\\LexerSpec.flex");
+    public static final Path PARSER_SPEC_PATH = Paths.get("src\\miniJava\\parser\\ParserSpec.cup");
 
     /**
      * Caminho para os arquivos de teste
      */
     static final Path TEST_FILES_DIR = Paths.get("test");
 
-    public static void main(String[] args) throws IOException {
-        for (Path testFile : Files.newDirectoryStream(TEST_FILES_DIR)) {
-            System.out.println(testFile.getFileName());
-            Lexer.main(new String[]{testFile.toString()});
-        }
+    public static void main(String[] args) throws Exception {
+        gen();
+    }
+
+    /**
+     * Gera as classes do lexer e parser automaticamente
+     */
+    public static void gen() {
+        Gen.genLexer(LEXER_SPEC_PATH, LEXER_SPEC_PATH.getParent());
+        Gen.genParser(PARSER_SPEC_PATH, PARSER_SPEC_PATH.getParent(), "Parser", "Tokens");
     }
 }
